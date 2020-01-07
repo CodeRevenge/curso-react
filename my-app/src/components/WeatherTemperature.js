4e2ed25e684ed12deb0b61bf8706f71811edd927
CodeRevenge <mas.arley2009@hotmail.com>
2020-01-07 00:13:15 -0600

    components

---
+++ my-app/src/components/WeatherTemperature.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const WeatherTemperature = () => (
+    <div>
+        <span>
+            12 C°
+        </span>
+    </div>
+);
+
+export default WeatherTemperature;
