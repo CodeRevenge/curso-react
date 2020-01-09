67f7dc61b5a6f3dbdeb9f2b9d32de50ccf030ec9
CodeRevenge <mas.arley2009@hotmail.com>
2020-01-08 23:58:33 -0600

    cities map

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,12 +4,21 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import LocationList from './components/LocationList'
 import './App.css';
 
+const cities = [
+  'Guadalajara,mx',
+  'Zamora,mx',
+  'Bucaramanga,col',
+  'Buenos Aires,ar',
+  'Bogota,col',
+  'Ciudad de Mexico,mx'
+]
+
 class App extends Component{
   render() {
     return (
       <MuiThemeProvider>
         <div className="App">
-          <LocationList />
+          <LocationList cities={cities}/>
         </div>
       </MuiThemeProvider>
     );
